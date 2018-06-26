e6de0b8547a46380447ce9167af482f547e0c1d1
Xavier Serrano <zombispormedio007@gmail.com>
2018-06-26 21:09:47 +0200

    configure proto

---
+++ packages/automate-storage-search/index.js
@@ -0,0 +1,21 @@
+const drivelist = require('drivelist')
+// const prettyBytes = require('pretty-bytes')
+const { inspect } = require('util')
+// const path = require('path')
+// const fs = require('fs')
+
+drivelist.list((error, drives) => {
+  if (error) {
+    throw error
+  }
+
+  console.log(inspect(drives, { depth: null }))
+
+  // const [ , usb ] = drives
+  // const {  mountpoints: [ { path: dir } ], size } = usb
+  // console.log(prettyBytes(size))
+  /* const filePath = path.resolve(dir, 'hello.txt');
+  const writeStream = fs.createWriteStream(filePath);
+  writeStream.write("hello");
+  writeStream.end(); */
+})
--- packages/automate-torrent-download/index.js
+++ packages/automate-torrent-download/index.js
@@ -1,21 +1,17 @@
-const drivelist = require('drivelist');
-const prettyBytes = require('pretty-bytes');
-const { inspect } = require('util');
-const path = require('path');
-const fs = require('fs');
+const http = require('http')
+const port = process.env.PORT
 
-drivelist.list((error, drives) => {
-  if (error) {
-    throw error;
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
   }
- 
-  console.log(inspect(drives, { depth: null }));
 
-  const [ , usb] = drives;
-  const { mountpoints: [ { path: dir } ], size } = usb;
-  console.log(prettyBytes(size))
-  /* const filePath = path.resolve(dir, 'hello.txt');
-  const writeStream = fs.createWriteStream(filePath);
-  writeStream.write("hello");
-  writeStream.end(); */
-});
+  console.log(`server is listening on ${port}`)
+})
